f91b202acff3b9b0004939bda91196379a7a8eb9
scaulfield01 <s.caulfield88@gmail.com>
2016-02-28 19:00:49 -0800

    add spinner

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 $( document ).ready(function() {
 var arryLocal = 1
 var thumbApiUrl="http://jsonplaceholder.typicode.com/photos";
+var opts = {
+  lines: 11, // The number of lines to draw
+  length: 15, // The length of each line
+  width: 10, // The line thickness
+  radius: 30, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb
+  speed: 0.6, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: 'auto', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+};
+var spinner = null;
+var spinner_div = 0;
 
-//load ajax call when the page is loaded
+
 AjaxThumbNails();
-// unHideBatch();
 
 //click event ajax call
   $('.results-button').click(function(){
-    debugger
     unHideBatch();
   });
 
-//methods structure
-// ajaxcall(loopAddThumb(AddThumb(scoreGenerate)))
-
   function AjaxThumbNails(){
+    loadSpinner();
     $.ajax({
       url:thumbApiUrl,
       dataType:"JSON",
@@ -23,6 +39,7 @@ AjaxThumbNails();
       success: function(resp){loopAddThumbnail(resp)},
       error: function(err){alert('Error: '+ err)}
     });
+
   }
 
   function loopAddThumbnail(resp){
@@ -32,6 +49,7 @@ AjaxThumbNails();
      var thumbnailId = resp[i].id;
       addThumbnail(thumbnailUrl,thumbnailId)
     };
+    spinner.stop(spinner_div);
     unHideBatch()
   };
 
@@ -56,7 +74,14 @@ AjaxThumbNails();
       $(element).show()
       arryLocal += 1
     };
-
+  }
+  function loadSpinner(){
+    spinner_div = $('#spinner').get(0);
+    if(spinner == null) {
+    spinner = new Spinner(opts).spin(spinner_div);
+    } else {
+    spinner.spin(spinner_div);
+    }
   }
 
 });
